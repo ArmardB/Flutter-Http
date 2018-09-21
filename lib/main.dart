44411528866f5d3634d362eb7e7dac5ca2ecdd89
Armard Bellamy <armardbellamy@gmail.com>
2018-09-20 21:39:01 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'ui/flutter_parse.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Flutter Parse',
+    home: new FlutterParse(),
+  ));
+}
